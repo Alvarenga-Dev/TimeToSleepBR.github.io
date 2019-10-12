ee066e4b10e270f421e4e583e43cae9a4d21254d
Pedro <joaopedroaats7@gmail.com>
2019-10-12 12:01:44 -0300

    Fix btn Pontuacão geral

--- js/quiz/score.js
+++ js/quiz/score.js
@@ -6,7 +6,7 @@ totalScoreList.innerHTML = totalScoreTeste
   .map(score => {
     return `
 
-    <li class="high-score"><span style="color: #54d0dd;"> ${score.score}</span> - ${score.name} | ${score.email} </li>
+    <li class="high-score"><span style="color: #54d0dd;"> ${score.score}</span> - ${score.name}<span style="color: #54d0dd;"> | </span>  ${score.mail} </li>
 
     `;
   })
